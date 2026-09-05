ff8301580157c1ab31b9e9c2af171736e5a4f399
kapasam2000-pixel <kapasam2000@gmail.com>
2026-09-05 19:47:02 +0200

    fix: register dashboard operational maintenance in WSGI

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,7 @@ import os
 
 from dashboard_auth import configure_dashboard_auth
 from dashboard_compat import register_dashboard_compat
+from dashboard_operations import register_operations
 from web_app_v2 import app
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -11,5 +12,6 @@ if os.environ.get("AIVF_TRUST_PROXY", "0") == "1":
 
 configure_dashboard_auth(app)
 register_dashboard_compat(app)
+register_operations(app)
 
 __all__ = ["app"]
