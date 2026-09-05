f75bc8b4f01197d1b702a7d05ea9494a53a173c2
kapasam2000-pixel <kapasam2000@gmail.com>
2026-09-05 19:44:45 +0200

    fix: register authenticated dashboard compatibility routes

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@
 import os
 
 from dashboard_auth import configure_dashboard_auth
+from dashboard_compat import register_dashboard_compat
 from web_app_v2 import app
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -9,5 +10,6 @@ if os.environ.get("AIVF_TRUST_PROXY", "0") == "1":
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1)
 
 configure_dashboard_auth(app)
+register_dashboard_compat(app)
 
 __all__ = ["app"]
